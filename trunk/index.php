dbffd286842b31469a1e8a49a488690eab3c5e68
ashameev@mail.ru <ashameev@mail.ru@873b31e2-57d4-9afc-9ef2-73562bd02c67>
2011-05-16 10:23:06 +0000

    index

---
+++ trunk/index.php
@@ -0,0 +1,64 @@
+<?php
+/**
+* @version		$Id: index.php,v 1.11 2009/09/11 08:24:33 shameev Exp $
+* @package		MZPortal.Framework
+* @subpackage	Factory
+* @copyright	Copyright (C) 2009 МИАЦ ИО
+* @license		GNU/GPL, see LICENSE.php
+* This version may have been modified pursuant
+* to the GNU General Public License, and as distributed it includes or
+* is derivative of works licensed under the GNU General Public License or
+* other free or open source software licenses.
+* See COPYRIGHT.php for copyright notices and details. 
+*/
+
+// на время разработки отображаем все ошибки
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+// Верхний уровень 
+define( '_MZEXEC', 1 );
+define( 'MZPATH_BASE', dirname(__FILE__) );
+define( 'DS', DIRECTORY_SEPARATOR );
+
+require_once ( MZPATH_BASE .DS.'includes'.DS.'factory.php' );
+
+$mainframe = new MZFactory();
+    // Проверяем авторизован ли пользователь
+$mainframe->check_auth();
+$mainframe->get_acl();
+    // Загружаем основной шаблон
+$mainframe->get_layout();
+    // Отображаем текущего пользователя и завершение сессии
+$mainframe->set_user_pane();
+    // Загружаем приложение
+$mainframe->set_content();
+    // Устанавливаем текущее приложение
+$mainframe->set_route();
+    // Загружаем сообщения
+$mainframe->set_message_queue();
+    // Загружаем панель инструментов
+$mainframe->set_toolbar();
+    // Определяем заголовок страницы
+$mainframe->set_title();
+    // Загружаем панель задач (иерархия объектов)
+//$mainframe->set_task_pane();
+    // Загрузка javascript'ов
+$mainframe->set_javascripts();
+    // Загрузка стилей
+$mainframe->set_css();
+    // Сводим воедино
+$mainframe->render();
+    // Отправляем на вывод
+echo $mainframe->site;
+//$r = Registry::getInstance();
+//print_r($r);
+//$s = SessionStorage::getInstance();
+//print_r($s);
+//$c = Constraint::getInstance();
+//print_r($c);
+//echo '<br />Сессия: ';
+//print_r($_SESSION);
+//print_r($_POST);
+//print_r($_FILES);
+ 
+?>

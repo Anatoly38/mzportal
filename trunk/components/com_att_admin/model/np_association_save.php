a592c156e0baad0c8c7277e71df3803a15e71980
shameev38 <shameev38@873b31e2-57d4-9afc-9ef2-73562bd02c67>
2014-07-07 08:08:00 +0000

    m.log

---
+++ trunk/components/com_att_admin/model/np_association_save.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* @version      $Id$
+* @package      MZPortal.Framework
+* @subpackage   AttAdmin
+* @copyright    Copyright (C) 2009-2014 МИАЦ ИО
+
+Прямой доступ запрещен
+*/
+defined( '_MZEXEC' ) or die( 'Restricted access' );
+require_once ( MZPATH_BASE .DS.'components'.DS.'item_save.php' );
+
+class NPAssociationSave extends ItemSave
+{
+    protected $model = 'NPAssociationQuery';
+    
+    public function get_post_values()
+    {
+        $this->query->наименование  = Request::getVar('наименование');
+        $this->query->аббревиатура  = Request::getVar('аббревиатура');
+    }
+}
+?>

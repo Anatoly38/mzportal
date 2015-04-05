696af0295b17dc1e7c2e3659e91d1b7ad4baf759
shameev38 <shameev38@873b31e2-57d4-9afc-9ef2-73562bd02c67>
2015-04-05 11:50:08 +0000

    m.log

--- trunk/components/com_lpu/model/lpu_query.php
+++ trunk/components/com_lpu/model/lpu_query.php
@@ -1,9 +1,9 @@
 <?php
 /**
-* @version		$Id$
-* @package		MZPortal.Framework
-* @subpackage	Passport_LPU
-* @copyright	Copyright (C) 2011 МИАЦ ИО
+* @version      $Id$
+* @package      MZPortal.Framework
+* @subpackage   Passport_LPU
+* @copyright    Copyright (C) 2009-2015 МИАЦ ИО
 
 Прямой доступ запрещен
 */
